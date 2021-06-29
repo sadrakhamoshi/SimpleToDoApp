63f3a9117227f174715069b45c02d4a71b73d25e
sadrakhamoshi <s.a.d.kh.121@gmail.com>
2021-06-29 10:50:29 +0430

    crete android project

---
+++ to-do-app/app/src/main/java/com/example/todo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.todo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void AddTask(View view) {
+    }
+}
---
+++ to-do-app/app/src/main/java/com/example/todo/models/Item.java
@@ -0,0 +1,13 @@
+package com.example.todo.models;
+
+public class Item {
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String name) {
+        this.title = name;
+    }
+}
